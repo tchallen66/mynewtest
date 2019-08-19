f166373e7367d88e8b2b8fb0d83bf7d1d31fdcf7
tchallen66 <tchallen66@gmail.com>
2019-08-19 11:01:17 +0100

    Update example.tf

--- example.tf
+++ example.tf
@@ -3,6 +3,11 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+resource "aws_eip" "ip" {
+    vpc = true
+    instance = aws_instance.example.id
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
